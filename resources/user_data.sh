876f8ad7de75cd611a86a1201d1f37923679e94d
Renuka Fernando <renukapiyumal@gmail.com>
2019-10-14 12:52:12 +0530

    add buildspec.yml and user_data.sh to install java and CodeDeploy agent

---
+++ resources/user_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# On Linux
+# install CodeDeploy agent
+yum -y update
+yum install -y ruby
+yum install -y aws-cli
+cd /home/ec2-user
+aws s3 cp s3://aws-codedeploy-us-east-2/latest/install . --region us-east-2
+chmod +x ./install
+./install auto
+
+# install java
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
+yum -y install jdk-8u131-linux-x64.rpm
